376d89087c2b85d05b6d3827f5cee914663c3c87
BrunoRodri <fidbruno@hotmail.com>
2025-04-04 16:42:36 -0300

    Commit final

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="desafio_banco",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="bruno_rodrigues",
+    author_email="fidbruno@hotmail.com",
+    description="Desafio Banco - Projeto de Python DIO",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/BrunoRodri/simple-package-template",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
